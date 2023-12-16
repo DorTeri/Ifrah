104f1201ee280ade2bc2a9c6fc2862b5fe81152b
DorTeri <dortayari@gmail.com>
2023-12-16 13:23:55 +0200

    Images fix

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -78,7 +78,7 @@ const page = (props: Props) => {
             <div className={`${isLoading && 'hidden'} grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4`}>
               {
                 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(i => (
-                  <div key={i} className='transition-opacity duration-500 ease-in-out hover:opacity-75'>
+                  <div key={i} className='transition-opacity w-full h-full duration-500 ease-in-out hover:opacity-75'>
                     <ImagePreview
                       url={getImagePath(`image_${i}`)} />
                   </div>
